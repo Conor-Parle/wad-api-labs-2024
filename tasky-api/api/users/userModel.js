c0edbc79846c6446e5f2e3107f0758db5feddac1
Conor Parle <conorparle@conors-air.home>
2024-05-19 01:33:45 +0100

    Upcoming movies from TMDB

--- tasky-api/api/users/userModel.js
+++ tasky-api/api/users/userModel.js
@@ -4,7 +4,14 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   username: { type: String, unique: true, required: true},
-  password: {type: String, required: true }
+  password: {type: String, required: true,
+    validate: {
+        validator: function(v) {
+            return /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/.test(v);
+        },
+        message: props => `${props.value} is not a valid password! It must be at least 8 characters long, contain at least one letter, one number, and one special character.`
+    }
+}
 });
 
 export default mongoose.model('User', UserSchema);
